b484370e6b1f109a281e7790fa681b04abff0ea3
Owen Mellema <owen.mellema@cerner.com>
2020-10-15 23:05:23 -0500

    Add CellContainer

---
+++ src/main/java/org/architectdrone/archevo/universe/cellcontainer/CellContainer.java
@@ -0,0 +1,14 @@
+package org.architectdrone.archevo.universe.cellcontainer;
+
+import java.util.List;
+import org.architectdrone.archevo.cell.Cell;
+
+public interface CellContainer {
+    int getSize();
+    void set(int x, int y, Cell cell) throws Exception;
+    Cell get(int x, int y);
+    void delete(int x, int y);
+    List<Cell> getAll();
+    List<CellPosition> getAllPositions();
+    void load(List<CellPosition> cells) throws Exception;
+}
