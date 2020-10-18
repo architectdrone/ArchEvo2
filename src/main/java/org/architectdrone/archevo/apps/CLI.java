1308da9e57c3e9b30d7e1fbc747d2cca7088229f
Owen Mellema <owenmllm@gmail.com>
2020-10-18 16:26:39 -0500

    Add display for highest lineage

--- src/main/java/org/architectdrone/archevo/apps/CLI.java
+++ src/main/java/org/architectdrone/archevo/apps/CLI.java
@@ -2,9 +2,11 @@ package org.architectdrone.archevo.apps;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
+
+import org.architectdrone.archevo.cell.Cell;
 import org.architectdrone.archevo.combathandler.CaptureTheFlag;
+import org.architectdrone.archevo.combathandler.CaptureTheFlagPercentage;
 import org.architectdrone.archevo.isa.asia.ASIA;
 import org.architectdrone.archevo.reproductionhandler.SetCost;
 import org.architectdrone.archevo.runner.Task;
@@ -15,37 +17,34 @@ import org.architectdrone.archevo.universe.Universe;
 public class CLI {
     public static void main(String[] args) throws Exception {
         Task update = new Task((a) -> {
-            List<Integer> lineages = a.getCellContainer().getAll().stream().map((cell) -> cell.cellStats.lineage).sorted().collect(Collectors.toList());
-            Integer longest_lineage = null;
-            if (lineages.size() > 0)
+            Cell longest_lineage_cell = getLongestLineageCell(a.getCellContainer().getAll());
+            Cell oldest_cell = getOldestCell(a.getCellContainer().getAll());
+            Cell most_viral_cell = getMostViralCell(a.getCellContainer().getAll());
+
+            if (oldest_cell != null)
             {
-                longest_lineage = lineages.get(lineages.size()-1);
+                System.out.println(
+                        "I: "+ a.getNumberOfInterations() +
+                                " ORG: " + a.getCellContainer().getAll().size() +
+                                " LIN: "+ longest_lineage_cell.cellStats.lineage +
+                                " AGE: " +oldest_cell.cellStats.age +
+                                " VIR: " +most_viral_cell.cellStats.virility);
+
+                if (longest_lineage_cell.cellStats.lineage >= 2)
+                {
+                    System.out.println("The cell with the longest lineage has the following genome:");
+                    printCell(most_viral_cell);
+                }
             }
-            List<Integer> ages = a.getCellContainer().getAll().stream().map((cell) -> cell.cellStats.age).sorted().collect(Collectors.toList());
-            Integer eldest = null;
-            if (ages.size() > 0)
-            {
-                eldest = ages.get(ages.size()-1);
-            }
-            List<Integer> virilities = a.getCellContainer().getAll().stream().map((cell) -> cell.cellStats.virility).sorted().collect(Collectors.toList());
-            Integer most_virile = null;
-            if (virilities.size() > 0)
-            {
-                most_virile = virilities.get(virilities.size()-1);
-            }
-            System.out.println(
-                    "I: "+ a.getNumberOfInterations() +
-                    " ORG: " + a.getCellContainer().getAll().size() +
-                    " LIN: "+longest_lineage +
-                    " AGE: " +eldest +
-                    " VIR: " +most_virile);
+
         }, 10000);
 
         Universe universe = new Universe(new ASIA(),
                 10,
                 IterationExecutionMode.INSTRUCTION_BY_INSTRUCTION,
-                0,
-                new CaptureTheFlag(),
+                1,
+                1,
+                new CaptureTheFlagPercentage(),
                 new SetCost(),
                 0.001f,
                 5,
@@ -55,6 +54,82 @@ public class CLI {
         taskList.add(update);
         UniverseRunner runner = new UniverseRunner(universe, taskList);
 
-        runner.runForNIterations(1000000);
+        runner.runForNIterations(100000000);
+    }
+
+    public static Cell getMostViralCell(List<Cell> cells)
+    {
+        List<Integer> virilities = cells.stream().map((cell) -> cell.cellStats.virility).sorted().collect(Collectors.toList());
+        final Integer most_virile;
+        if (virilities.size() > 0)
+        {
+            most_virile = virilities.get(virilities.size()-1);
+        }
+        else
+        {
+            most_virile = null;
+        }
+
+        if (most_virile == null)
+        {
+            return null;
+        }
+        else
+        {
+            return cells.stream().filter((a) -> a.cellStats.virility == most_virile).collect(Collectors.toList()).get(0);
+        }
+    }
+
+    public static Cell getOldestCell(List<Cell> cells)
+    {
+        List<Integer> ages = cells.stream().map((cell) -> cell.cellStats.age).sorted().collect(Collectors.toList());
+        final Integer highest_age;
+        if (ages.size() > 0)
+        {
+            highest_age = ages.get(ages.size()-1);
+        }
+        else
+        {
+            highest_age = null;
+        }
+
+        if (highest_age == null)
+        {
+            return null;
+        }
+        else
+        {
+            return cells.stream().filter((a) -> a.cellStats.age == highest_age).collect(Collectors.toList()).get(0);
+        }
+    }
+
+    public static Cell getLongestLineageCell(List<Cell> cells)
+    {
+        List<Integer> lineages = cells.stream().map((cell) -> cell.cellStats.lineage).sorted().collect(Collectors.toList());
+        final Integer longest_lineage;
+        if (lineages.size() > 0)
+        {
+            longest_lineage = lineages.get(lineages.size()-1);
+        }
+        else
+        {
+            longest_lineage = null;
+        }
+
+        if (longest_lineage == null)
+        {
+            return null;
+        }
+        else
+        {
+            return cells.stream().filter((a) -> a.cellStats.lineage == longest_lineage).collect(Collectors.toList()).get(0);
+        }
+    }
+
+    public static void printCell(Cell cell) {
+        for (Integer gene : cell.getGenome())
+        {
+            System.out.println(new ASIA().binaryToString(gene));
+        }
     }
 }
